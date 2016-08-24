d55856650068f7aec2503d3a1dbca41199497a34
Andrew <andrew.vinocur@gmail.com>
2016-08-23 17:08:38 -0700

    checkpoint: logging embed URL to the console

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,28 @@
-$(document).on("ready", function(){
 
+
+
+
+$(document).on("ready", function(){
+  $('#submit').on('click', function(event){
+    event.preventDefault(); /// prevents page from reloading
+    console.log('atleast the button works');
+    $.ajax({
+        method: "GET",
+        url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC", /// http://api.giphy.com/v1/gifs/search ///
+        data: "json", ///$("form").serialize(),
+        success: onSuccess,
+        error: onError,
+    });
+      function onSuccess(json) {
+        $("div").append("<h1>"+json.title+"</h1>"); /// what does json do
+        console.log(json.data[0].embed_url);
+      }
+    function onError(xhr, status, errorThrown) {
+        alert("Sorry, there was a problem!");
+        console.log("Error: " + errorThrown);
+        console.log("Status: " + status);
+        console.dir(xhr);
+    }
+  });
+  /// append gifs to h1 or somewhere else on the page
 });
